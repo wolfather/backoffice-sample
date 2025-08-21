bb30eb30acbb65622de730269275f53a43a03eed
wolfather <so.israelweb@gmail.com>
2025-08-21 19:48:13 -0300

    add persistence dashboard

--- src/app/pages/Dashboard/Dashboard.tsx
+++ src/app/pages/Dashboard/Dashboard.tsx
@@ -1,53 +1,59 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { User } from "../../components/User/User";
 import type { UserEntity } from "../../components/User/user.entity";
 import { useGetData } from "../../hooks/useGetData";
 import { Paginator } from "../../components/Paginator/Paginator";
-import { memo, useMemo, useState } from "react";
+import { memo, useContext, useMemo, useState } from "react";
+import { SessionContext } from "../../providers/session";
 
 function DashboardPage() {
     const [currentPage, setCurrentPage] = useState<number>(1);
-    
+    const navigate = useNavigate()    
     const params = useMemo(() => ({'page': String(currentPage)}), [currentPage]);
 
+    const { sessionToken } = useContext(SessionContext)
     const { data, loading, err, errMessage } = useGetData<UserEntity[]>({
         path: 'USERS', 
         params,
     });
 
+    if(!sessionToken) {
+        navigate('/');
+    } else {
 
-    if (loading) {
-        return (
-            <div>loading...</div>
-        );
-    }
-    if (err) {
-        return (
-            <div>{errMessage}</div>
-        )
-    }
+        if (loading) {
+            return (
+                <div>loading...</div>
+            );
+        }
+        if (err) {
+            return (
+                <div>{errMessage}</div>
+            )
+        }
 
-    if (data.data) {
-        return (
-            <div>
-                <h1>Dashboard</h1>
-
+        if (data.data) {
+            return (
                 <div>
-                    {data?.data.map(user => (
-                        <User key={user.id} data={user} />
-                    ))}
+                    <h1>Dashboard</h1>
 
-                    <Paginator
-                        totalRecords={data.total}
-                        recordsPerPage={data.per_page}
-                        onPageChange={setCurrentPage}
-                        currentPage={currentPage}
-                    />
+                    <div>
+                        {data?.data.map(user => (
+                            <User key={user.id} data={user} />
+                        ))}
+
+                        <Paginator
+                            totalRecords={data.total}
+                            recordsPerPage={data.per_page}
+                            onPageChange={setCurrentPage}
+                            currentPage={currentPage}
+                        />
+                    </div>
+
+                    <Outlet />
                 </div>
-
-                <Outlet />
-            </div>
-        )
+            )
+        }
     }
 
     return (
