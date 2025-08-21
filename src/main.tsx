777c7b6ed8ce66ddcd29483a68b056019310f85c
wolfather <so.israelweb@gmail.com>
2025-08-21 17:57:19 -0300

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -32,7 +32,6 @@ createRoot(document.getElementById('root')!).render(
         <RouterProvider router={router} />
       </UserPreferenceProvider>
     </SessionProvider>
-    
   </StrictMode>,
 )
 
